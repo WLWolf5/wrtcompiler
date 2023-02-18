4755e7a32e9ff1e73f3cddab0d527daa1f4af376
WLWolf5 <782558181@qq.com>
2023-02-18 22:36:05 +0800

    update

--- lede-diy-part2.sh
+++ lede-diy-part2.sh
@@ -4,7 +4,7 @@
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-rm -rf package/feeds/packages/net/miniupnpd
+rm -rf feeds/packages/net/miniupnpd
 
 # Openwrt扩展软件包
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/openwrt-packages/luci-app-vlmcsd
